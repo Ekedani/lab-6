6eecebaa1b0c405e3d44d5b2d6e0fe09a59d0c1d
Vladlen <ilusenkovladlen@gmail.com>
2021-06-05 18:23:39 +0300

    Частично перенес код для работы с .bmp

--- work-with-bmp/bitmapRender.cpp
+++ work-with-bmp/bitmapRender.cpp
@@ -3,3 +3,41 @@
 void bitmapRender::writeToFile(const std::string& filePath) {
 
 }
+
+Pixel *&bitmapRender::operator[](int index) {
+    return pixels[index];
+}
+
+bitmapRender::bitmapRender(int32_t width, int32_t depth) {
+    this->width = width;
+    this->depth = depth;
+    pixels = new Pixel*[depth];
+    for (int i = 0; i < depth; ++i) {
+        pixels[i] = new Pixel[width];
+    }
+}
+
+void bitmapRender::writePixelsToFile(const std::string &address) const {
+    int delta = 4 - ((width * 3) % 4);
+    std::ofstream file (address, std::ios::out | std::ios::binary | std::ios::app);
+    for(int i = 0; i < depth; i++){
+        this->writeLine(file, i);
+        for(int j = 0; j < delta; j++){
+            uint8_t zero = 0;
+            file.write((char*)&zero, sizeof(uint8_t));
+        }
+    }
+    file.close();
+}
+
+void bitmapRender::writeLine(std::ofstream &file, int proceededLines) const {
+    for (int counter = 0; counter < width; ++counter) {
+        //Запись цвета пикселя
+        uint8_t red = pixels[proceededLines][counter].redComponent;
+        file.write((char*)&red, sizeof(uint8_t));
+        uint8_t green = pixels[proceededLines][counter].greenComponent;
+        file.write((char*)&green, sizeof(uint8_t));
+        uint8_t blue = pixels[proceededLines][counter].blueComponent;
+        file.write((char*)&blue, sizeof(uint8_t));
+    }
+}
--- work-with-bmp/bitmapRender.h
+++ work-with-bmp/bitmapRender.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <fstream>
 
 struct Pixel{
     uint8_t redComponent;
@@ -10,7 +11,31 @@ struct Pixel{
 
 class bitmapRender {
 private:
+    //Размеры файла
+    int32_t filesize;
+    int32_t width;
+    int32_t depth;
+
+    //Информация о пикселях
+    Pixel** pixels;
+
+    //Запись массива пикселей в файл
+    void writePixelsToFile(const std::string& address) const;
+
+    //Запись строки массива в файл
+    void writeLine(std::ofstream &file, int proceededLines) const;
 
 public:
+    //Запись в .bmp файл
     void writeToFile(const std::string& filePath);
+
+    //Оператор индексации, чтобы тебе жилось проще
+    Pixel*& operator[](int index);
+
+    //TODO: вспомнить как вычислялся filesize
+    bitmapRender(int32_t width, int32_t depth);
+
+    void writeHeader(const std::string& filePath){
+
+    }
 };
