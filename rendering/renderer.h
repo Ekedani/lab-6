10e38cf072f43bf41ae4dfe01f1581dfb2b402c1
Vladlen <ilusenkovladlen@gmail.com>
2021-06-06 14:54:18 +0300

    Created Renderer and LightSource classes

---
+++ rendering/renderer.h
@@ -0,0 +1,25 @@
+#include "../work-with-bmp/bitmapRender.h"
+#include "../data-struct/triangle.h"
+
+struct LightSource{
+    uint8_t redComponent;
+    uint8_t greenComponent;
+    uint8_t blueComponent;
+
+    Point* sourcePoint;
+
+    //Белый цвет
+    explicit LightSource(Point* sourcePoint);
+
+    //Любой цвет
+    LightSource(Point* sourcePoint, uint16_t R, uint16_t G, uint16_t B);
+};
+
+//В теории будет содержать всю логику рендеринга
+class Renderer{
+private:
+
+public:
+
+};
+
