86df8bd94051ee776fcf94913efa114d290ca514
koziuberda <koziuberdaseverin@gmail.com>
2021-06-06 09:26:17 +0300

    Vector class is created

--- data-struct/triangle.h
+++ data-struct/triangle.h
@@ -11,3 +11,29 @@ struct Triangle {
     Point* secondVertex;
     Point* thirdVertex;
 };
+
+class Vector3 {
+    double xCoord;
+    double yCoord;
+    double zCoord;
+public:
+    Vector3(double x, double y, double z) {
+        xCoord = x;
+        yCoord = y;
+        zCoord = z;
+    }
+    Vector3(const Point& start, const Point& end) {
+        xCoord = end.xCoord - start.xCoord;
+        yCoord = end.yCoord - start.yCoord;
+        zCoord = end.zCoord - start.zCoord;
+    }
+    static double dot(const Vector3& v1, const Vector3& v2) {
+        return (v1.xCoord*v2.xCoord) + (v1.yCoord*v2.yCoord) + (v1.zCoord*v2.zCoord);
+    }
+    static Vector3 cross(const Vector3& v1, const Vector3& v2) {
+        double x = v1.yCoord*v2.zCoord - v1.zCoord*v2.yCoord;
+        double y = v1.zCoord*v2.xCoord - v1.xCoord*v2.zCoord;
+        double z = v1.xCoord*v2.yCoord - v1.yCoord*v2.xCoord;
+        return Vector3(x,y,z);
+    }
+};
