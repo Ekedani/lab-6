d738b411f8013f61fae15db24e7bc0f3756c799f
Vladlen <ilusenkovladlen@gmail.com>
2021-06-07 16:43:07 +0300

    Reworked splitNodes methods

--- r-tree/r_tree.cpp
+++ r-tree/r_tree.cpp
@@ -1,15 +1,14 @@
 #include "r_tree.h"
 
 void rTree::findObjectsUsingRay(Line curRay, Node *curNode, vector<Triangle *> &result) {
-    if(curNode->isLeaf()){
-        for (auto & object : curNode->objects) {
-            if(curRay.triangle_intersection(*object->triangle) != 0){
+    if (curNode->isLeaf()) {
+        for (auto &object : curNode->objects) {
+            if (curRay.triangle_intersection(*object->triangle) != 0) {
                 result.push_back(object->triangle);
             }
         }
-    }
-    else{
-        for (auto & node : curNode->nodes) {
+    } else {
+        for (auto &node : curNode->nodes) {
             //TODO:
             //if(curRay.doesIntersect(node->MBP)){
             //    findObjectsUsingRay(curRay, node, result);
@@ -19,16 +18,15 @@ void rTree::findObjectsUsingRay(Line curRay, Node *curNode, vector<Triangle *> &
 }
 
 std::vector<Triangle *> rTree::findObjectsUsingRay(Line curRay) {
-    std::vector<Triangle*> result;
-    if(root->isLeaf()){
-        for (auto & object : root->objects) {
-            if(curRay.triangle_intersection(*object->triangle) != 0){
+    std::vector<Triangle *> result;
+    if (root->isLeaf()) {
+        for (auto &object : root->objects) {
+            if (curRay.triangle_intersection(*object->triangle) != 0) {
                 result.push_back(object->triangle);
             }
         }
-    }
-    else{
-        for (auto & node : root->nodes) {
+    } else {
+        for (auto &node : root->nodes) {
             //TODO:
             //if(curRay.doesIntersect(node->MBP)){
             //    findObjectsUsingRay(curRay, node, result);
@@ -42,7 +40,7 @@ void rTree::splitNotLeafNode(Node *curNode, Node *insertedNode) {
 
     Node *nodeParent;
     //В случае если является корнем дерева
-    if(curNode == root){
+    if (curNode == root) {
         nodeParent = new Node;
         curNode->parentNode = nodeParent;
         nodeParent->nodes.push_back(curNode);
@@ -50,68 +48,68 @@ void rTree::splitNotLeafNode(Node *curNode, Node *insertedNode) {
         root->parentNode = nullptr;
     }
     //В случае если он им не является
-    else{
+    else {
         nodeParent = curNode->parentNode;
     }
 
     //Копирование узлов
-    Node **newNodes = new Node*[maxCount + 1];
-    for (int i = 0; i < maxCount; ++i){
+    Node **newNodes = new Node *[maxCount + 1];
+    for (int i = 0; i < maxCount; ++i) {
         newNodes[i] = curNode->nodes[i];
     }
     newNodes[maxCount] = insertedNode;
 
-    //bool axisIsX = !splitNotLeafAxis(curNode, insertedNode);
+    //Указатели для хранения лучшего распределения
+    Node *firstNode;
+    Node *secondNode;
+    Node *minimalFirstNode = new Node;
+    Node *minimalSecondNode = new Node;
 
-    Node* firstNode;
-    Node* secondNode;
-    Node* minimalFirstNode = new Node;
-    Node* minimalSecondNode = new Node;
-
-    /*double minimalOverlap = DBL_MAX;
-    double minimalArea = DBL_MAX;
+    //Параметры сравнения
+    auto minimalOverlap = DBL_MAX;
+    auto minimalVolume = DBL_MAX;
     double curOverlap;
-    double curArea;
+    double curVolume;
 
-    for (int i = 0; i < 1; ++i) {
-        sortForNotLeaf(newNodes, axisIsX, i);
-        for (int j = minCount - 1; j < maxCount - minCount; ++j) {
-            firstNode = new Node;
-            secondNode = new Node;
+    //Сортировка нод
+    qsort(newNodes, maxCount + 1, sizeof(Node*), sortNodesByAxis);
 
-            int curNode;
-            for (curNode = 0; curNode <= j ; curNode++) {
-                firstNode->nodes.push_back(newNodes[curNode]);
-            }
+    //Выбор лучшего распределения
+    for (int j = minCount - 1; j < maxCount - minCount; ++j) {
+        firstNode = new Node;
+        secondNode = new Node;
 
-            for (curNode = j;  curNode < maxCount - 1; curNode++) {
-                secondNode->nodes.push_back(newNodes[curNode]);
-            }
+        int curNode;
+        for (curNode = 0; curNode <= j; curNode++) {
+            firstNode->nodes.push_back(newNodes[curNode]);
+        }
 
-            firstNode->updateMBR();
-            secondNode->updateMBR();
+        for (curNode = j; curNode < maxCount - 1; curNode++) {
+            secondNode->nodes.push_back(newNodes[curNode]);
+        }
 
-            curOverlap = firstNode->MBR.overlap(&secondNode->MBR);
+        firstNode->updateMBP();
+        secondNode->updateMBP();
 
-            if(curOverlap < minimalOverlap){
-                *(minimalFirstNode) = *(firstNode);
-                *(minimalSecondNode) = *(secondNode);
-                minimalOverlap = curOverlap;
-            }
-            else{
-                if (curOverlap == minimalOverlap){
-                    curArea = firstNode->MBR.area() + secondNode->MBR.area() - curOverlap;
-                    if(curArea < minimalArea){
-                        *(minimalFirstNode) = *(firstNode);
-                        *(minimalSecondNode) = *(secondNode);
-                        minimalArea = curArea;
-                    }
+        curOverlap = firstNode->MBP.overlapVolume(secondNode->MBP);
+
+        if (curOverlap < minimalOverlap) {
+            *minimalFirstNode = *firstNode;
+            *minimalSecondNode = *secondNode;
+            minimalOverlap = curOverlap;
+        } else {
+            if (curOverlap == minimalOverlap) {
+                curVolume = firstNode->MBP.volume() + secondNode->MBP.volume() - curOverlap;
+                if (curVolume < minimalVolume) {
+                    *minimalFirstNode = *firstNode;
+                    *minimalSecondNode = *secondNode;
+                    minimalVolume = curVolume;
                 }
             }
-            delete firstNode;
-            delete secondNode;
         }
-    }*/
+        delete firstNode;
+        delete secondNode;
+    }
 
     minimalFirstNode->parentNode = curNode->parentNode;
     minimalSecondNode->parentNode = curNode->parentNode;
@@ -119,14 +117,13 @@ void rTree::splitNotLeafNode(Node *curNode, Node *insertedNode) {
     *curNode = *minimalFirstNode;
     delete minimalFirstNode;
 
-    if(curNode->parentNode->nodes.size() < maxCount){
+    if (curNode->parentNode->nodes.size() < maxCount) {
         curNode->parentNode->nodes.push_back(minimalSecondNode);
-        while(nullptr != nodeParent){
+        while (nullptr != nodeParent) {
             nodeParent->updateMBP();
             nodeParent = nodeParent->parentNode;
         }
-    }
-    else{
+    } else {
         splitNotLeafNode(curNode->parentNode, minimalSecondNode);
     }
 }
@@ -134,72 +131,70 @@ void rTree::splitNotLeafNode(Node *curNode, Node *insertedNode) {
 void rTree::splitLeafNode(Node *curNode, TriangleLeaf *curObj) {
     Node *nodeParent;
     //В случае если является корнем дерева
-    if(curNode == root){
+    if (curNode == root) {
         nodeParent = new Node;
         curNode->parentNode = nodeParent;
         nodeParent->nodes.push_back(curNode);
         root = nodeParent;
         root->parentNode = nullptr;
     }
-    //В случае если он им не является
-    else{
+        //В случае если он им не является
+    else {
         nodeParent = curNode->parentNode;
     }
 
-    auto **newObjects = new TriangleLeaf*[maxCount + 1];
-    for (int i = 0; i < maxCount; ++i){
+    auto **newObjects = new TriangleLeaf *[maxCount + 1];
+    for (int i = 0; i < maxCount; ++i) {
         newObjects[i] = curNode->objects[i];
     }
     newObjects[maxCount] = curObj;
 
-    Node* firstNode;
-    Node* secondNode;
-    Node* minimalFirstNode = new Node;
-    Node* minimalSecondNode = new Node;
+    Node *firstNode;
+    Node *secondNode;
+    Node *minimalFirstNode = new Node;
+    Node *minimalSecondNode = new Node;
 
     auto minimalOverlap = DBL_MAX;
-    auto minimalArea = DBL_MAX;
+    auto minimalVolume = DBL_MAX;
     double curOverlap;
-    double curArea;
+    double curVolume;
 
-    qsort(newObjects, maxCount + 1, sizeof(TriangleLeaf*), sortObjectsByAxis);
+    qsort(newObjects, maxCount + 1, sizeof(TriangleLeaf *), sortObjectsByAxis);
+    for (int j = minCount - 1; j <= maxCount - minCount; ++j) {
 
-    //for (int j = minCount - 1; j <= maxCount - minCount; ++j) {
+        firstNode = new Node;
+        secondNode = new Node;
 
-    //    firstNode = new Node;
-    //    secondNode = new Node;
+        int curPlace = 0;
+        for (curPlace; curPlace <= j; curPlace++) {
+            firstNode->objects.push_back(newObjects[curPlace]);
+        }
+        for (curPlace; curPlace <= maxCount; curPlace++) {
+            secondNode->objects.push_back(newObjects[curPlace]);
+        }
 
-    //    int curPlace = 0;
-    //    for (curPlace; curPlace <= j; curPlace++) {
-    //        firstNode->objects.push_back(newObjects[curPlace]);
-    //    }
-    //    for (curPlace; curPlace <= maxCount; curPlace++) {
-    //        secondNode->objects.push_back(newObjects[curPlace]);
-    //    }
+        firstNode->updateMBP();
+        secondNode->updateMBP();
 
-    //    firstNode->updateMBP();
-    //    secondNode->updateMBP();
+        curOverlap = firstNode->MBP.overlapVolume(secondNode->MBP);
 
-    //    curOverlap = firstNode->MBR.overlap(&secondNode->MBR);
-
-    //    if(curOverlap < minimalOverlap){
-    //        *minimalFirstNode = *firstNode;
-    //        *minimalSecondNode = *secondNode;
-    //        minimalArea = curArea;
-    //    }
-    //    else{
-    //        if(curOverlap == minimalOverlap){
-    //            curArea = firstNode->MBP.volume() + secondNode->MBP.volume() - curOverlap;
-    //            if(curArea < minimalArea){
-    //                *minimalFirstNode = *firstNode;
-    //                *minimalSecondNode = *secondNode;
-    //                minimalArea = curArea;
-    //            }
-    //        }
-    //    }
-    //    delete firstNode;
-    //    delete secondNode;
-    //}
+        if (curOverlap < minimalOverlap) {
+            *minimalFirstNode = *firstNode;
+            *minimalSecondNode = *secondNode;
+            minimalVolume = curVolume;
+        } else {
+            if (curOverlap == minimalOverlap) {
+                curVolume = firstNode->MBP.volume() + secondNode->MBP.volume() - curOverlap;
+                if (curVolume < minimalVolume) {
+                    *minimalFirstNode = *firstNode;
+                    *minimalSecondNode = *secondNode;
+                    minimalVolume = curVolume;
+                }
+            }
+        }
+        delete firstNode;
+        delete secondNode;
+    }
 
     minimalFirstNode->parentNode = curNode->parentNode;
     minimalSecondNode->parentNode = curNode->parentNode;
@@ -207,21 +202,20 @@ void rTree::splitLeafNode(Node *curNode, TriangleLeaf *curObj) {
     *curNode = *minimalFirstNode;
     delete minimalFirstNode;
 
-    if(curNode->parentNode->nodes.size() < maxCount){
+    if (curNode->parentNode->nodes.size() < maxCount) {
         curNode->parentNode->nodes.push_back(minimalSecondNode);
-        while(nullptr != nodeParent){
+        while (nullptr != nodeParent) {
             nodeParent->updateMBP();
             nodeParent = nodeParent->parentNode;
         }
-    }
-    else{
+    } else {
         splitNotLeafNode(curNode->parentNode, minimalSecondNode);
     }
 }
 
 int rTree::sortObjectsByAxis(const void *a, const void *b) {
-    const TriangleLeaf *arg1 = *(const TriangleLeaf **)a;
-    const TriangleLeaf *arg2 = *(const TriangleLeaf **)b;
+    const TriangleLeaf *arg1 = *(const TriangleLeaf **) a;
+    const TriangleLeaf *arg2 = *(const TriangleLeaf **) b;
     /*if(arg1->MBP == arg2->MBP){
         return 0;
     }
@@ -234,8 +228,8 @@ int rTree::sortObjectsByAxis(const void *a, const void *b) {
 }
 
 int rTree::sortNodesByAxis(const void *a, const void *b) {
-    const Node *arg1 = *(const Node **)a;
-    const Node *arg2 = *(const Node **)b;
+    const Node *arg1 = *(const Node **) a;
+    const Node *arg2 = *(const Node **) b;
     /*if(arg1->MBP == arg2->MBP){
         return 0;
     }
