9de14fdee5655c664ed0264aec0421331f713455
koziuberda <koziuberdaseverin@gmail.com>
2021-06-07 15:18:44 +0300

    Volume method for parallelepiped is implemented

--- r-tree/prism.h
+++ r-tree/prism.h
@@ -2,11 +2,19 @@
 
 class Prism {
 private:
-    Point firstPoint;
-    Point secondPoint;
+    Point* firstPoint;
+    Point* secondPoint;
 public:
 
-    double volume(){
+    Prism(Point* p1, Point* p2){
+        firstPoint = p1;
+        secondPoint = p2;
+    }
 
+    double volume() const{
+        double a = secondPoint->xCoord - firstPoint->xCoord;
+        double b = secondPoint->yCoord - firstPoint->yCoord;
+        double c = secondPoint->zCoord - firstPoint->zCoord;
+        return a*b*c;
     }
 };
