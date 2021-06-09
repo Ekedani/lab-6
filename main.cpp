09f40dffacab5bc3e5b76f14c1110bb30731f57b
Vladlen <ilusenkovladlen@gmail.com>
2021-06-09 15:59:15 +0300

    Fixed minor bug with bmp size

--- main.cpp
+++ main.cpp
@@ -7,42 +7,42 @@ using namespace std;
 
 int main() {
     //Пока что работает только с кратными 4 размерами, потом пофиксим
-    const int size = 256;
-    vector<Triangle*> testVec;
+    const int size = 999;
+    vector<Triangle *> testVec;
     testVec = objFileReader::readTriangles(/*Insert path to file*/"G:\\my-repos\\lab-6\\cow.obj");
 
     rTree testTree;
 
-    for (auto & i : testVec) {
+    for (auto &i : testVec) {
         testTree.insertTriangle(i);
     }
 
     bitmapRender test(size, size);
 
-    Point CameraPos{0,0, -5};
+    Point CameraPos{0, 0, -5};
     Point CameraDir{0, 0, 1};
     Point PlaneOrigin{CameraPos.xCoord + CameraDir.xCoord,
-                      CameraPos.yCoord+CameraDir.yCoord,
-                      CameraPos.zCoord+CameraDir.zCoord};
+                      CameraPos.yCoord + CameraDir.yCoord,
+                      CameraPos.zCoord + CameraDir.zCoord};
     double fov = 60;
 
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
 
-            test[i][j].redComponent =  255;
+            test[i][j].redComponent = 255;
             test[i][j].greenComponent = 255;
             test[i][j].blueComponent = 255;
 
-            double xCoord = (i - size/2)/(double)size;
-            double yCoord = -(j - size/2)/(double)size;
+            double xCoord = (i - size / 2) / (double) size;
+            double yCoord = -(j - size / 2) / (double) size;
 
             double distanceToPlaneFromCamera = CameraPos.distanceTo(PlaneOrigin);
-            double fovInRad = (fov/180)*3.14159265;
+            double fovInRad = (fov / 180) * 3.14159265;
             double realPlaneHeight = distanceToPlaneFromCamera * tan(fovInRad);
             Point positionOnPlane{
-                    PlaneOrigin.xCoord + xCoord*realPlaneHeight/2,
-                    PlaneOrigin.yCoord + yCoord*realPlaneHeight/2,
-                    PlaneOrigin.zCoord+0
+                    PlaneOrigin.xCoord + xCoord * realPlaneHeight / 2,
+                    PlaneOrigin.yCoord + yCoord * realPlaneHeight / 2,
+                    PlaneOrigin.zCoord + 0
             };
 
             Line ray;
@@ -51,7 +51,7 @@ int main() {
 
 
             if (!testTree.findObjectsUsingRay(ray).empty()) {
-                test[i][j].redComponent =  0;
+                test[i][j].redComponent = 0;
                 test[i][j].greenComponent = 0;
                 test[i][j].blueComponent = 255;
             }
